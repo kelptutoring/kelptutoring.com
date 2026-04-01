1021ebb5a83261617b10944742b67ba2e2b5b8ee
Kelp Tutoring <thiago.loyola@kelptutoring.com>
2026-03-31 21:57:45 -0300

    Update login.js

--- src/auth/login.js
+++ src/auth/login.js
@@ -16,7 +16,7 @@ form?.addEventListener('submit', async (event) => {
   }
 
   if (result.ok) {
-    window.location.href = '../login/student-dashboard.html'
+    window.location.href = './student-dashboard.html'
   }
 })
 
